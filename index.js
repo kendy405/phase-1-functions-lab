2adab5f16512df1e8fb4c3b4432e5eecea884a39
Irene <irene.muthomi@student.moringaschool.com>
2023-09-24 17:54:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+let scruberHq =42
+function distanceFromHqInBlocks(blocks){
+     return Math.abs (scruberHq - blocks);
+
+  }
+  function distanceFromHqInFeet(feet){
+    return Math.abs((scruberHq - feet) * 264);
+  }
+
+  function distanceTravelledInFeet(num1, num2){
+    return Math.abs((num1 - num2) * 264);
+  }
+  function calculatesFarePrice(start,destination){
+    let distance = Math.abs(destination - start) *264;
+        if(distance < 400){
+            return 0;
+        }else if(distance < 2000){
+            return 0.02 * (distance - 400) ;
+        } else if(distance < 2500){
+            return 25;
+        }else if(distance > 2500){
+            return 'cannot travel that far';
+        }
+  }
